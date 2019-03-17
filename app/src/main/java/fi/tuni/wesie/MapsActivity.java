101efc86fd9ac42aeb6355c273efc2498074d6e5
Toni Vänttinen <toni.vanttinen@tuni.fi>
2019-03-17 15:47:47 +0200

    Place info
    Can click a place now and get info and show it in a custom window

--- app/src/main/java/fi/tuni/wesie/MapsActivity.java
+++ app/src/main/java/fi/tuni/wesie/MapsActivity.java
@@ -11,16 +11,22 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PointOfInterest;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.FetchPlaceRequest;
+import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.libraries.places.widget.Autocomplete;
 import com.google.android.libraries.places.widget.AutocompleteActivity;
 import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
@@ -28,10 +34,14 @@ import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import java.util.Arrays;
 import java.util.List;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnPoiClickListener {
 
     private GoogleMap mMap;
     private LocationManager manager;
+    private PlacesClient placesClient;
+    private String clikedPlace;
 
     /**
      *
@@ -48,49 +58,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Places
         Places.initialize(getApplicationContext(), "AIzaSyCB59BRagw7ZSAhen1IO_rSjRCMsjAEvaw");
-    }
-
-    /*
-    private void search() {
-        imgButton.((TextView v, int actionId, KeyEvent event) -> {
-            int AUTOCOMPLETE_REQUEST_CODE = 1;
-
-            // Set the fields to specify which types of place data to
-            // return after the user has made a selection.
-            List<Place.Field> fields = Arrays.asList(Place.Field.ID, Place.Field.NAME);
-
-            // Start the autocomplete intent.
-            Intent intent = new Autocomplete.IntentBuilder(
-                    AutocompleteActivityMode.FULLSCREEN, fields)
-                    .build(this);
-            startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE);
-
-            if (actionId == EditorInfo.IME_ACTION_SEARCH || actionId == EditorInfo.IME_ACTION_DONE) {
-                locate();
-            }
-
-            return false;
-        });
-    }
-    */
-
-    private void locate() {
-        /*
-        String search = mSearchText.getText().toString();
-        Geocoder coder = new Geocoder(MapsActivity.this);
-        List<Address> list = new ArrayList<>();
-        try {
-            list = coder.getFromLocationName(search,1);
-        } catch (IOException e) {
-            Log.d("locate()","not working " + e.getMessage());
-        }
-
-        if(list.size() > 0) {
-            Address address = list.get(0);
-            Log.d("Osote", address.toString());
-            moveCamera(new LatLng(address.getLatitude(),address.getLongitude()), 14f, address.getFeatureName());
-        }
-        */
+        placesClient = Places.createClient(this);
     }
 
     /**
@@ -115,19 +83,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         getLocationPermission();
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
         mMap.setMyLocationEnabled(true);
         mMap.getUiSettings().setMyLocationButtonEnabled(false);
         SetOnClick();
+        mMap.setOnPoiClickListener(this);
+        moveHome();
+        mMap.setInfoWindowAdapter(customInfo());
+    }
+
+    private GoogleMap.InfoWindowAdapter customInfo() {
+        GoogleMap.InfoWindowAdapter result = new GoogleMap.InfoWindowAdapter() {
+            @Override
+            public View getInfoWindow(Marker marker) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+                // Getting view from the layout file info_window_layout
+                View v = getLayoutInflater().inflate(R.layout.windowlayout, null);
+                LatLng latLng = marker.getPosition();
+                TextView info = v.findViewById(R.id.Info);
+                info.setText(clikedPlace);
+                return v;
+            }
+        };
+        return result;
     }
 
     /**
      *
      */
     private void moveHome() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
         Location loc = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
@@ -141,7 +132,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private void SetOnClick() {
         mMap.setOnMapClickListener((LatLng arg0) -> {
             Log.d("SetOnClick", arg0.toString());
-            // TODO: set marker on the clicked location
+            mMap.clear();
+            MarkerOptions mark = new MarkerOptions().position(arg0);
+            mMap.addMarker(mark);
         });
     }
 
@@ -149,8 +142,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      *
      */
     private void getLocationPermission() {
-        if (ActivityCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        if (ActivityCompat.checkSelfPermission(MapsActivity.this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MapsActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MapsActivity.this, new String[]{ACCESS_FINE_LOCATION}, 1);
             return;
         }
     }
@@ -198,5 +191,39 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 // The user canceled the operation.
             }
         }
+    }
+
+    @Override
+    public void onPoiClick(PointOfInterest poi) {
+        // Define a Place ID.
+        String placeId = poi.placeId;
+
+        // Specify the fields to return.
+        List<Place.Field> placeFields = Arrays.asList(Place.Field.ID,
+                Place.Field.NAME,
+                Place.Field.PHONE_NUMBER,
+                Place.Field.PRICE_LEVEL,
+                Place.Field.RATING);
+
+        // Construct a request object, passing the place ID and fields array.
+        FetchPlaceRequest request = FetchPlaceRequest.builder(placeId, placeFields).build();
+
+        placesClient.fetchPlace(request).addOnSuccessListener((response) -> {
+            Place place = response.getPlace();
+            Log.d("onPoiClick", ": " + place.toString());
+            mMap.clear();
+            clikedPlace = new String (place.getName() + "\n" +
+                            "Rating: " + place.getRating() + "\n" +
+                            "Price level: " + place.getPriceLevel() + "\n" +
+                            "Phone number: " + place.getPhoneNumber());
+            Marker mark = mMap.addMarker(new MarkerOptions().position(poi.latLng));
+            mark.showInfoWindow();
+        }).addOnFailureListener((exception) -> {
+            if (exception instanceof ApiException) {
+                ApiException apiException = (ApiException) exception;
+                // Handle error with given status code.
+                Log.e("onPoiClick", "Place not found: " + exception.getMessage());
+            }
+        });
     }
 }
